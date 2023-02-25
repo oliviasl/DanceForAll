0abb8e74f36d538cec7c269e7d223bb4e40893f4
oliviasl <oslai@usc.edu>
2023-02-25 13:16:56 -0800

    Initial files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./routes/Home";
+
+function App() {
+  return <div>
+    <Router>
+      <Routes>
+        <Route exact path = "/" element={<Home />}/>
+      </Routes>
+    </Router>
+    </div>
+}
+
+export default App;
